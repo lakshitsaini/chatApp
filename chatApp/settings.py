ac520ed5db046ca7d1fcea8eafa76e36223ddcc5
lakshitsaini <lakshitsaini2017@gmail.com>
2021-06-01 18:18:06 +0530

    redis enabled

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -72,8 +72,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatApp.wsgi.application'
 ASGI_APPLICATION = 'chatApp.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
